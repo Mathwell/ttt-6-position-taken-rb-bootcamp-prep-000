de5011c23197df1393f6d7899808048d322cfbb8
Olena Ageyeva <eageeva@hotmail.com>
2017-09-24 15:08:17 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,9 @@
 # code your #position_taken? method here!
 def position_taken(board, number)
   index=number.to_i-1
+  if board.length==0 || index<0
+    return nil
+  end
   if board[index].strip==" "
     return false
   else
