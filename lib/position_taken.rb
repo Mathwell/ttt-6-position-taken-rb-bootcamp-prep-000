c215f041997d21710fa27874e4765a6aaea61e5a
Olena Ageyeva <eageeva@hotmail.com>
2017-09-24 15:18:05 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,11 +3,11 @@ def position_taken?(board, number)
   index=number.to_i-1
   puts board[index]
   puts board[index]=="X" || board[index]=="O" || board[index]=="x" || board[index]=="o"
-  puts  board[index].strip==" "
+  puts  board[index]==" "
   if board.length==0 || index<0 || board[index]==nil
     return false
   end
-  if board[index].strip==" "
+  if board[index]==" "
     return false
   elsif board[index]=="X" || board[index]=="O" || board[index]=="x" || board[index]=="o"
     return true
